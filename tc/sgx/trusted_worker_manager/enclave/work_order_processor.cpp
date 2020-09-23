24f4ba29174e061f48d43d610fb96151b85c58be
pegartillo95 <pegarc03@ucm.es>
2020-09-23 10:48:07 +0200

    Changing keepState to a new parameter and examples to work with it

--- tc/sgx/trusted_worker_manager/enclave/work_order_processor.cpp
+++ tc/sgx/trusted_worker_manager/enclave/work_order_processor.cpp
@@ -104,6 +104,11 @@ namespace tcf {
             "workloadId",
             "invalid request; failed to retrieve work load id");
 
+        keepState = GetJsonStr(
+            params_object,
+            "keepState",
+            "invalid request; failed to retrieve keep state");
+
         requester_id = GetJsonStr(
             params_object,
             "requesterId",
@@ -287,13 +292,13 @@ namespace tcf {
             ByteArray workload_bytes = HexStringToBinary(workload_id);
             std::string workload_type(workload_bytes.begin(), workload_bytes.end());
 
-            if(workload_type.substr(workload_type.size() - 10) == "_keepState"){
+            /*if(workload_type.substr(workload_type.size() - 10) == "_keepState"){
                  keepState = "true";
                  workload_type = workload_type.substr(0, workload_type.size() - 10);
             }
              else {
                  keepState = "false";
-            }
+            }*/
 
             WorkloadProcessor *processor = \
                 WorkloadProcessor::CreateWorkloadProcessor(workload_type, worker_id, keepState);
