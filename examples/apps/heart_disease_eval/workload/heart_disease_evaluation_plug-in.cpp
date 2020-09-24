66aad7c1b3adadba3162b6a8c3afb134c40536d7
pegartillo95 <pegarc03@ucm.es>
2020-09-24 18:12:21 +0200

    Trying to fix statefulness

--- examples/apps/heart_disease_eval/workload/heart_disease_evaluation_plug-in.cpp
+++ examples/apps/heart_disease_eval/workload/heart_disease_evaluation_plug-in.cpp
@@ -23,13 +23,9 @@
 
 REGISTER_WORKLOAD_PROCESSOR("heart-disease-eval",HeartDiseaseEval)
 
-std::unique_ptr<HeartDiseaseEvalLogic> heart_disease_eval_logic(new HeartDiseaseEvalLogic());
+HeartDiseaseEvalLogic* heart_disease_eval_logic(new HeartDiseaseEvalLogic());
 
-HeartDiseaseEval::HeartDiseaseEval() {
-    heart_disease_eval_logic.release();
-    std::unique_ptr<HeartDiseaseEvalLogic> heart_disease_eval_logic_new(new HeartDiseaseEvalLogic());
-    heart_disease_eval_logic = heart_disease_eval_logic_new.release();
-}
+HeartDiseaseEval::HeartDiseaseEval() {}
 
 HeartDiseaseEval::~HeartDiseaseEval() {}
 
